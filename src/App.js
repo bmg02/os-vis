fda259842d317af9f4f44c924483b51d48e12e32
Bhuvan Gandhi <bhuvan12501@gmail.com>
2020-09-18 14:40:25 +0530

    Updated header

--- src/App.js
+++ src/App.js
@@ -2,25 +2,60 @@ import React from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
 import './App.scss';
 import { createBrowserHistory } from 'history';
-
+import { MuiThemeProvider } from '@material-ui/core';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
 import { Header, Home, Algo } from './pages';
+import scssVariables from './App.scss';
 
 const history = createBrowserHistory();
 
+let theme = responsiveFontSizes(createMuiTheme({
+	palette: {
+		primary: {
+			main: scssVariables.primary,
+			contrastText: '#444'
+		},
+		secondary: {
+			main: scssVariables.secondary,
+			contrastText: '#444'
+		},
+		danger: {
+			main: scssVariables.danger,
+			contrastText: '#444'
+		},
+		warning: {
+			main: scssVariables.warning,
+			contrastText: '#444'
+		},
+		info: {
+			main: scssVariables.info,
+			contrastText: '#444'
+		}
+	},
+	typography: {
+		h4: {
+			fontFamily: 'Montserrat-Bold',
+			color: '#444'
+		}
+	}
+}));
+
 function App() {
 	return (
-		<Router history={history}>
-			<Header />
-			<Route render={({ location }) => (
-				<div className='main-container'>
+		<MuiThemeProvider theme={theme}>
+			<Router history={history}>
+				<Header />
+				<Route render={({ location }) => (
+					<div className='main-container'>
 						<Switch location={ location }>
 							<Route exact path='/' component={Home} />
 							<Route exact path='/:algo' component={Algo} />
 						</Switch>
-				</div>
-			)} />
-		</Router>
+					</div>
+				)} />
+			</Router>
+		</MuiThemeProvider>
 	);
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Home() {
     return (
-        <div>Home</div>
+        <div className='container-div flex-layout'>
+            <div className='section-div grey-div flex-30'>Scheduling Algorithms</div>
+            <div className='section-div flex-auto'>How to use?</div>
+        </div>
     );
 }
 
